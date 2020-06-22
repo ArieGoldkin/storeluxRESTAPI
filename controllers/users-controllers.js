f8e3e9372faaea0f30d259a1d9123ee589a49583
ArieGoldkin <ariegoldkin88@gmail.com>
2020-06-22 14:27:18 +0300

    password bcypt and web token added to user, image upload fix

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-errors");
 const User = require("../models/user");
@@ -51,12 +53,20 @@ const signup = async (req, res, next) => {
 
   const defaultImagePath = "uploads\\images\\user.png";
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create user, please try again", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     firstName,
     lastName,
     email,
     image: defaultImagePath,
-    password,
+    password: hashedPassword,
     products: [],
   });
 
@@ -67,7 +77,21 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      "supersecret_string_gold_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -76,7 +100,9 @@ const login = async (req, res, next) => {
   let existingUser;
 
   try {
-    existingUser = await User.findOne({ email });
+    existingUser = await User.findOne({
+      email,
+    });
   } catch (err) {
     const error = new HttpError(
       "Logging in failed, please try again later",
@@ -85,17 +111,50 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password != password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid email or password, could not login.",
-      401 
+      401
     );
     return next(error);
   }
 
+  let isValidPassword = false;
+  try {
+    //compares the password with the hashed password in the database returns boolean value
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log in, please check your credentials and try again",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid email or password, could not login.",
+      401
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "supersecret_string_gold_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Logging in failed, please try again.", 500);
+    return next(error);
+  }
+
   res.json({
-    message: "Logged in!",
-    user: existingUser.toObject({ getters: true }),
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
   });
 };
 
@@ -135,7 +194,6 @@ const updateUserInfo = async (req, res, next) => {
   }
   const { firstName, lastName, email, address, image, phone } = req.body;
 
-  const defaultImagePath = "uploads\\images\\user.png";
   const userId = req.params.uid;
   let user;
 
@@ -149,16 +207,18 @@ const updateUserInfo = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = image;
+
   user.firstName = firstName;
   user.lastName = lastName;
   user.email = email;
   user.address = address;
   user.phone = phone;
   try {
-    if (image != defaultImagePath) {
+    if (user.image != imagePath) {
       user.image = req.file.path;
     } else {
-      user.image = defaultImagePath;
+      user.image = image;
     }
   } catch (err) {
     const error = new HttpError(
