02f6f8a98067d6178fba4c1bbd008739a8ea549a
ArieGoldkin <ariegoldkin88@gmail.com>
2020-10-01 01:01:44 +0300

    added admin production id

--- middleware/adminInfo.js
+++ middleware/adminInfo.js
@@ -1,4 +1,7 @@
 module.exports = {
-  adminId: "5f4e3d4e4b5be605d08e907b",
+  adminId: "5f57f6303c979f0017e41109",
   email: "ariegoldkin88@gmail.com",
 };
+
+//production adminId: "5f57f6303c979f0017e41109"
+//development adminId: "5f4e3d4e4b5be605d08e907b"
