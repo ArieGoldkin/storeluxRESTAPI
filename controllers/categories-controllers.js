8b563234112412291247c34a53fda227677a838f
ArieGoldkin <ariegoldkin88@gmail.com>
2020-10-10 12:44:28 +0300

    category route fix

--- controllers/categories-controllers.js
+++ controllers/categories-controllers.js
@@ -1,9 +1,10 @@
-const { validationResult } = require("express-validator");
+// const { validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 
 const HttpError = require("../models/http-errors");
 const Category = require("../models/category");
 
+//GET CATEGORIES
 const getCategories = async (req, res, next) => {
   let categories;
   try {
@@ -22,35 +23,4 @@ const getCategories = async (req, res, next) => {
   });
 };
 
-const createCategory = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new HttpError(
-      "Invalid inputs passed, please check your data.",
-      422
-    );
-    return next(error);
-  }
-
-  const { name } = req.body;
-  const createdCategory = new Category({
-    name,
-  });
-
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    createdCategory.save({ session: sess });
-    await sess.commitTransaction();
-  } catch (err) {
-    const error = new HttpError(
-      "Creating category failed, please try again.",
-      500
-    );
-    return next(error);
-  }
-  res.status(201).json({ category: createdCategory });
-};
-
 exports.getCategories = getCategories;
-exports.createCategory = createCategory;
--- routes/categories-routes.js
+++ routes/categories-routes.js
@@ -1,20 +1,9 @@
 const express = require("express");
-const { check } = require("express-validator");
 
 const categoriesControllers = require("../controllers/categories-controllers");
-
-const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/", categoriesControllers.getCategories);
-
-router.use(checkAuth);
-
-router.post(
-  "/addcategory",
-  [check("name").not().isEmpty()],
-  categoriesControllers.createCategory
-);
 
 module.exports = router;
