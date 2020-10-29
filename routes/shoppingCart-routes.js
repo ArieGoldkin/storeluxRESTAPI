192a27c537cc2e2dabaa6d38d03a7576087210a8
ArieGoldkin <ariegoldkin88@gmail.com>
2020-10-30 00:14:40 +0200

    typo fix

--- routes/shoppingCart-routes.js
+++ routes/shoppingCart-routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { check } = require("express-validator");
+// const { check } = require("express-validator");
 
 const shoppingCartControllers = require("../controllers/shoppingCart-controllers");
-const fileUpload = require("../middleware/file-upload");
+// const fileUpload = require("../middleware/file-upload");
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
